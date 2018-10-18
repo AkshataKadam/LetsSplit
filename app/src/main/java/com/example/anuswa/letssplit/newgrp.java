14b25fdf5ed35f7f96c77dcbe8948a87390de68b
AkshataKadam <akshatakadam1908@gmail.com>
2018-10-18 16:56:34 +0530

    crashhhhh

--- app/src/main/java/com/example/anuswa/letssplit/newgrp.java
+++ app/src/main/java/com/example/anuswa/letssplit/newgrp.java
@@ -1,5 +1,6 @@
 package com.example.anuswa.letssplit;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
@@ -32,10 +33,14 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import com.google.firebase.database.Query;
+
 
 public class newgrp extends AppCompatActivity {
 
-    private EditText grpname;
+    private EditText grpname_edit;
     private Button addPer, save, back, next;
     private final int Pick_contact = 1;
     private ImageView grpimg;
@@ -43,6 +48,13 @@ public class newgrp extends AppCompatActivity {
     FirebaseUser firebaseUser;
     String ConName, grpName, uid, con;
     private TextView grp;
+
+    String  grpname,sresult,key;
+    String[] personname = new String[10];
+    String[] persondebt = new String[10];
+    int count=1,x=0;
+
+    String mtotbill,mcat,contactnm;
 
     ArrayAdapter<String> arrayAdapter;
     ArrayList<String> listg = new ArrayList<>();
@@ -56,7 +68,7 @@ public class newgrp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newgrp);
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        grpname = (EditText) findViewById(R.id.grpnm_id);
+        grpname_edit = (EditText) findViewById(R.id.grpnm_id);
         addPer = (Button) findViewById(R.id.addP_id);
        save = (Button) findViewById(R.id.save_id);
         grpimg = findViewById(R.id.grpimg);
@@ -79,7 +91,7 @@ public class newgrp extends AppCompatActivity {
                 startActivity(in);
             }
         });
-        next.setOnClickListener(new View.OnClickListener() {
+       /* next.setOnClickListener(new View.OnClickListener() {
             @Override
             //public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             public void onClick(View v) {
@@ -90,20 +102,100 @@ public class newgrp extends AppCompatActivity {
 
             }
 
+        });*/
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(newgrp.this);
+                View mview = getLayoutInflater().inflate(R.layout.category_dialog, null);
+                final EditText totalbill = mview.findViewById(R.id.idtotbill);
+                final EditText category = mview.findViewById(R.id.idcat);
+                mtotbill =totalbill.getText().toString();
+                mcat=category.getText().toString();
+
+
+                builder.setPositiveButton("Unequally", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        mtotbill =totalbill.getText().toString();
+                        mcat=category.getText().toString();
+
+                        //Toast.makeText(MainActivity.this, "added successfully", Toast.LENGTH_SHORT).show();
+                        if (!mtotbill.isEmpty() && !mcat.isEmpty())
+                        {
+                            startActivity(new Intent(newgrp.this, Distribute.class).putExtra("gn", grpname).putExtra("Result", sresult).putExtra("contactname", contactnm));
+                        }
+                        else
+                        {
+                            Toast.makeText(newgrp.this, "please fill all fields", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+                builder.setNegativeButton("Equally", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mtotbill =totalbill.getText().toString();
+                        mcat=category.getText().toString();
+
+
+                        if (!mtotbill.isEmpty() && !mcat.isEmpty())
+                        {
+                            x=x+1;
+                            Double result;
+                            try {
+                                double num = Double.parseDouble(mtotbill);
+                                result = num /count;
+                                sresult = String.valueOf(result);
+                                DisplayMembers();
+
+                                startActivity(new Intent(newgrp.this, ingrp.class));
+
+
+                            } catch (NumberFormatException e) {
+                            }
+                        }
+
+                        else
+                        {
+                            Toast.makeText(newgrp.this, "please fill all fields", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+
+
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.setView(mview);
+                AlertDialog dialog=builder.create();
+                dialog.show();
+            }
         });
+
+
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 //   Toast.makeText(newgrp.this,"You pressed save ",Toast.LENGTH_SHORT).show();
                 uid = firebaseUser.getUid();
-                grpName = grpname.getText().toString();
+                grpName = grpname_edit.getText().toString();
                 myref = FirebaseDatabase.getInstance().getReference("users").child(uid);
                 myref.child("Group").child(grpName).setValue(grpName);
 
                 grp.setText(grpName);
                 if (!grpName.isEmpty()) {
-                    grpname.setVisibility(View.INVISIBLE);
+                    grpname_edit.setVisibility(View.INVISIBLE);
                     grpimg.setVisibility(View.INVISIBLE);
                 }
             }
@@ -116,7 +208,9 @@ public class newgrp extends AppCompatActivity {
                 callContact(v);
 
             }
-        });    }
+        });
+
+    }
 
 
     public void callContact(View v) {
@@ -138,18 +232,18 @@ public class newgrp extends AppCompatActivity {
                     //String Conc=c.getString(c.getColumnIndexOrThrow(ContactsContract))
                     Toast.makeText(this, "You have picked " + name, Toast.LENGTH_LONG).show();
                     setCon(name);
-                    Retrive();
+                   //// Retrive();
                 }
             }
         }
     }
 
     void setCon(String name) {
-        String contactnm = name;
+        String contactnms = name;
         int cost = 1;
         //String connum = Contact;
         this.ConName = name;
-        List list = new List(contactnm,cost);
+        List list = new List(contactnms,cost);
         //list.setPerson(contactnm);
         getLayoutInflater();
         //this.con = Contact;
@@ -165,12 +259,12 @@ public class newgrp extends AppCompatActivity {
         grpref.child("users").child(mAuth.getCurrentUser().getUid()).child("Group").child(grpName).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Set<String> set=new HashSet<>();
+                Set<String> set = new HashSet<>();
                 Iterator iterator = dataSnapshot.getChildren().iterator();
 
                 while (iterator.hasNext()) {
-                   // List list =dataSnapshot.getValue();
-                    set.add(((DataSnapshot)iterator.next()).getKey());
+                    // List list =dataSnapshot.getValue();
+                    set.add(((DataSnapshot) iterator.next()).getKey());
                 }
 
                 listg.clear();
@@ -183,9 +277,71 @@ public class newgrp extends AppCompatActivity {
 
             }
         });
-
-
     }
+
+        private void DisplayMembers()
+        {
+
+            myref=FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getCurrentUser().getUid()).child("Group").child(grpname).child("Members");
+            myref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
+                    AmountInfo amountInfo=new AmountInfo(contactnm,sresult);
+
+                    int i=0;
+                    for(DataSnapshot ds: dataSnapshot.getChildren())
+                    {
+
+                        amountInfo.setPersonname(ds.getValue(AmountInfo.class).getPersonname());
+                        amountInfo.setPricee(ds.getValue(AmountInfo.class).getPricee());
+                        i=i+1;
+
+                        personname[i]=amountInfo.getPersonname();
+                        persondebt[i]=amountInfo.getPricee();
+
+                        if(x>0) {
+
+                            Query ref = FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getCurrentUser().getUid()).child("Group").child(grpname).child("Members").orderByChild("personname").equalTo(contactnm);
+                            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot1) {
+
+                                    for (DataSnapshot childsnap : dataSnapshot.getChildren()) {
+                                        key = childsnap.getKey();
+                                        update(key, sresult);
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                        }
+
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+        private void update (String key , String sresult)
+        {
+            myref=FirebaseDatabase.getInstance().getReference();
+            myref=myref.child("Users").child(mAuth.getCurrentUser().getUid()).child("Group").child(grpname).child("Members");
+            myref.child(key).child("pricee").setValue(sresult);
+
+        }
+
+
+
 }
 
 
